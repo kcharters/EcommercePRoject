e5446f6814764cb58111067902b60294fdbfd97a
Rebecca Nettleton (1000041601) <NETTR2@student.op.ac.nz>
2020-09-22 14:37:47 +1200

    Finished home page layout

--- ecommerce-app/src/views/Home.vue
+++ ecommerce-app/src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="home container">
-    <h1>Home Page</h1>
+  <div class="home">
+        <Hero></Hero>
+        <products></products>
+          <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-md-6">
+            <img src="../assets/aboutUs.png" alt="" width="450">
+          </div>
+          <div class="col-md-6">
+            <h3>Learn More About us</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quaerat voluptate iusto? Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!
+            </p>
+             <button class="btn custom-btn btn-outline-primary mr-4">Browse Products</button>
+          </div>
+        </div>
+      </div>
+    
   </div>
 </template>
 
@@ -12,3 +28,4 @@ export default {
     }
 };
 </script>
+
