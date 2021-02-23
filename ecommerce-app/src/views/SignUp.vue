adce96826d596529b19f87176edeaba35b87c815
kcharters89 <chartkr1@student.op.ac.nz>
2021-02-24 12:28:14 +1300

    added password strength meter

--- ecommerce-app/src/views/SignUp.vue
+++ ecommerce-app/src/views/SignUp.vue
@@ -10,9 +10,15 @@
     </div>
     <div class="form-group">
      <label for="signupInputPassword1">Password</label>
-    <input type="password" class="form-control" v-model="password" placeholder="Password">
+
+    <input type="password" class="form-control" v-model="password" placeholder="Password" required>
+    <span v-if="score === 0">Use better password</span>
+    <span v-if="score === 1">Use better password</span>
+    <span v-if="score === 2">Use better password</span>
+     <password-meter :password="password" @score="onScore"/>
+
     </div>
-    <button type="submit" class="btn btn-primary" @click="signUp">Create Account</button>
+    <button type="submit" class="btn btn-primary" @click="signUp" :disabled='isDisabled'>Create Account</button>
     <div class="form-group">
     <span>Already have an Account? <a data-toggle="modal" data-target="#login" class="link" > Login here </a></span>
 </div>
@@ -23,27 +29,41 @@
 
 <script>
 import firebase from 'firebase';
+import passwordMeter from "vue-simple-password-meter";
 export default {
     name: "SignUp",
+    components: { passwordMeter },
     data(){
         return{
             email:'',
-            password: ''
-
+            password: null,
+            score: null
         };
     },
     methods: {
         signUp: function(){
             firebase.auth().createUserWithEmailAndPassword(this.email,this.password).then(
                  (user) => {
-                 this.$router.push('shop')
+           // var user = firebase.auth().currentUser;
+            this.$router.push('/')     
+            console.log(user)      
+
                 },
                 (err) => {
                     alert('Oh no!' + err.message)
                 }
             )
-        }
+        },
+       onScore({ score, strength }) {
+      this.score = score; 
     }
+    },
+     computed: {
+  	isDisabled: function(){
+    	return !this.password;
+
+    }
+  }
     
 };
 </script>
