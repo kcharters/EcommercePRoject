7f93f515c75f52669376ef0759fc07146bd95778
Rebecca Nettleton <nettr2@student.op.ac.nz>
2021-03-14 16:53:52 +1300

    Completed contact page design

--- ecommerce-app/src/views/ContactUs.vue
+++ ecommerce-app/src/views/ContactUs.vue
@@ -1,48 +1,7 @@
 <template>
   <div class="contactus">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">Contact Us</div>
-        <div class="card-body">
-          <form>
-               <div class="form-group row">
-                   <div class="col-md-6" >
-            <label for="firstname">First Name</label>
-            <input style="margin:5px"
-              type="text"
-              id="fistname"
-              name="firstname"
-              placeholder="Your first name.."
-            />
-                   </div>
-               </div>
-
-                <div class="form-group row">
-
-            <label for="lastname">Last Name</label>
-            <input
-            style="margin:5px"
-              type="text"
-              id="lastname"
-              name="lastname"
-              placeholder="Your last name.."
-            />
-            </div>
-             <div class="form-group row">
-            <label for="subject">Subject</label>
-            <textarea
-              id="subject"
-              name="subject"
-              placeholder="Your message"
-              style="height:200px"
-            ></textarea>
-             </div>
-
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-      </div>
-    </div>
+    <NavBar></NavBar>
+    <ContactForm></ContactForm>
   </div>
 </template>
 
@@ -56,4 +15,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
